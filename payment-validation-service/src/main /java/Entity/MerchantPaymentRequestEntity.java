fd23a0ca08de515a96120ce86f99319187f02a00
Surendra <131601968+Surendras4466@users.noreply.github.com>
2026-04-02 15:44:56 +0530

    Add files via upload

---
+++ payment-validation-service/src/main /java/Entity/MerchantPaymentRequestEntity.java
@@ -0,0 +1,19 @@
+package com.myproject.payments.Entity;
+
+import java.sql.Timestamp;
+
+import lombok.Data;
+
+@Data
+public class MerchantPaymentRequestEntity {
+
+    private Integer id;
+
+    private String endUserID;
+
+    private String merchantTxnReference;
+
+    private String transactionRequest;
+
+    private Timestamp creationDate;
+}
